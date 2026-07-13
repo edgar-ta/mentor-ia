2a8ede7cfdd953e383196c214480eb1eb0ebd4d7
Javier <franjavierggnrg@gmail.com>
2026-07-13 16:26:07 -0600

    Fix Aiven SSL self-signed certificate error

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -10,7 +10,7 @@ export const pool = mysql.createPool({
   database: process.env.DB_NAME || 'mentoria',
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
   ssl: {
-    rejectUnauthorized: true
+    rejectUnauthorized: false
   },
   waitForConnections: true,
   connectionLimit: 10,
