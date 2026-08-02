3d52f6fe160418e75de934e6038143287e139b98
Edgar Trejo Avila <edgar.avila.rro@gmail.com>
2026-08-02 16:56:08 -0600

    Changed root of vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: resolve(__dirname),
   plugins: [react()],
   server: {
     port: 5173,
     proxy: {
       '/api': 'http://localhost:4000'
     }
-  }
+  },
+
 });
